78daebd41f7ee1c4eab7128fa22ed76e590cb347
cpatarroyo <ca.patarroyo960@hotmail.com>
2025-01-25 18:42:26 -0500

    Added the space for a piechart and connected the frontend to the database query functions

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 import pandas as pd
+import matplotlib as mpl
+from dbFunc import nameSearch, ecSearch, pfamSearch, keggReactSearch, descriptionSearch
 
 app_ui = ui.page_fluid(
     ui.panel_title("Transcriptoma de Scedosporium apiospermum"),
     ui.layout_sidebar(
         ui.sidebar(
             ui.input_text('searchTerm', label='Termino de busqueda'),
-            ui.input_select('searchType', label='Buscar por:', choices=['Nombre', 'Anotación', 'Numero EC', 'Categoría Pfam', 'Reacción KEGG']),
+            ui.input_select('searchType', label='Buscar por:', choices=['Nombre', 'Descripción', 'Numero EC', 'Categoría Pfam', 'Reacción KEGG']),
             ui.input_switch('filter', label='Significancia'),
             ui.input_numeric('threshold', label='Umbral', min=0, max=1, step=0.01, value=0.05),
-            ui.input_action_button('search', label='Buscar')
+            ui.input_action_button('search', label='Buscar'),
+            ui.output_plot('difexp')
         ),
         ui.output_data_frame('resultsTab')
     )
@@ -18,8 +21,17 @@ app_ui = ui.page_fluid(
 def server(input: Inputs, output: Outputs, session: Session):
     @reactive.event(input.search)
     def getResults():
-        test = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
-        return test
+        if input.searchType() == 'Nombre':
+            results = nameSearch(input.searchTerm(), signif = input.filter(), signifThr = float(input.threshold()))
+        elif input.searchType() == 'Descripción':
+            results = descriptionSearch(input.searchTerm(), signif = input.filter(), signifThr = float(input.threshold()))
+        elif input.searchType() == 'Numero EC':
+            results = ecSearch(input.searchTerm(), signif = input.filter(), signifThr = float(input.threshold()))
+        elif input.searchType() == 'Categoría Pfam':
+            results = pfamSearch(input.searchTerm(), signif = input.filter(), signifThr = float(input.threshold()))
+        elif input.searchType() == 'Reacción KEGG':
+            results = keggReactSearch(input.searchTerm(), signif = input.filter(), signifThr = float(input.threshold()))
+        return results
 
     @render.data_frame
     def resultsTab():
